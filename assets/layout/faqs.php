472ca4e06654abbec8fba920574e48dc8aff5779
Heviaa7 <duwidevi657@gmail.com>
2025-01-26 20:38:08 +0700

    edit by hevia

---
+++ assets/layout/faqs.php
@@ -0,0 +1,55 @@
+<div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+      Apakah saya bisa mendaftar lebih dari satu lowongan?
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Setiap peserta hanya diperkenankan mendaftar satu lowongan magang saja. Kamu bisa kembali mendaftar lowongan magang lain apabila kamu dinyatakan tidak lolos pada salah satu lowongan magang sebelumnya.</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+      Bagaimana cara saya mendaftarkan diri?
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">
+        1. Buat akun untuk melanjutkan pendaftaran pada menu “Masuk ke Akun”. <br>
+        2. Setelah sudah buat akun, pergi ke menu "Lowongan".<br>
+        3. Pilih lowongan magang yang sesuai jurusan kamu yang sedang dibuka dengan cara klik tombol “Lihat detail”.<br>
+        4. Terakhir, kamu bisa melengkapi form pendaftaran yang tersedia.<br>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Apa yang harus saya lakukan setelah mendaftar magang?
+      </button>
+    </h2>
+    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">
+        1. Setelah mendaftar magang, kamu bisa menunggu notifikasi apakah kamu sesuai kriteria terhadap lowongan yang tersedia atau tidak. <br>
+        2. Jika kamu sesuai kriteria, maka kamu akan diberi link zoom untuk melakukan wawancara. <br>
+        3. Lalu kamu bisa menunggu kembali notifikasi email hasil wawancara kamu.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+        Apa yang harus saya lakukan setelah saya berhasil tahap wawancara?
+      </button>
+    </h2>
+    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">
+        Kamu harus melengkapi dokumen persyaratan lainnya, yang sudah ditentukan oleh instansi, seperti <br>
+        1. Surat Bangkesbangpol Provinsi <br>
+        2. Surat Bangkesbangpol Daerah <br>
+        3. Dan lain-lain, sesuai dengan persyaratan instansi.
+    </div>
+  </div>
+</div>
