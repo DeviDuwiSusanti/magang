87e172e7413e6ce4c75a80d1325f117d1450a3df
Heviaa7 <duwidevi657@gmail.com>
2025-01-29 22:44:29 +0700

    edit by hevia

--- user/edit_profil.php
+++ user/edit_profil.php
@@ -20,41 +20,47 @@
 
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="text" class="form-control" id="email" name="email" value="Heviaa" required>
+                <input type="email" class="form-control" id="email" name="email" value="Heviaa@gmail.com" required>
             </div>
 
             <div class="mb-3">
                 <label for="nik" class="form-label">NIK</label>
-                <input type="text" class="form-control" id="nik" name="nik" value="Heviaa" required>
+                <input type="text" class="form-control" id="nik" name="nik" value="35224233465274" required>
             </div>
 
             <div class="mb-3">
-                <label for="" class="form-label"></label>
-                <input type="text" class="form-control" id="" name="" value="Heviaa" required>
+                <label for="asal_studi" class="form-label">Asal Studi</label>
+                <input type="text" class="form-control" id="asal_studi" name="asal_studi" value="Heviaa" required>
             </div>
 
-            <!-- Alamat -->
-            <div class="mb-3">
-                <label for="alamat" class="form-label">Alamat</label>
-                <textarea class="form-control" id="alamat" name="alamat" rows="3" placeholder="Masukkan alamat lengkap" required></textarea>
-            </div>
-
-            <!-- Tempat Lahir -->
             <div class="mb-3">
                 <label for="tempat_lahir" class="form-label">Tempat Lahir</label>
-                <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir" placeholder="Masukkan tempat lahir" required>
+                <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir" value="Bojonegoro" required>
             </div>
 
             <!-- Tanggal Lahir -->
             <div class="mb-3">
                 <label for="tanggal_lahir" class="form-label">Tanggal Lahir</label>
-                <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" required>
+                <input type="date" class="form-control" id="tanggal_lahir" name="tanggal_lahir" value="2005-08-01" required>
+            </div>
+
+
+            <!-- Alamat -->
+            <div class="mb-3">
+                <label for="alamat" class="form-label">Alamat</label>
+                <textarea class="form-control" id="alamat" name="alamat" rows="3"  required>Ds.Rajawali, Kec.Kepoh, Kab.Ciamis</textarea>
+            </div>
+
+            <!-- Tempat Lahir -->
+            <div class="mb-3">
+                <label for="telepon" class="form-label">Telepon</label>
+                <input type="number" class="form-control" id="telepon" name="telepon" value="082487234" required>
             </div>
 
             <!-- Upload Image -->
             <div class="mb-3">
                 <label for="image" class="form-label">Foto Profil</label>
-                <input type="file" class="form-control" id="image" name="image" accept="image/*">
+                <input type="file" class="form-control" id="image" name="image" value="" accept="image/*">
             </div>
 
             <!-- Submit Button -->
