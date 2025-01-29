923b349230d0d94cea939fa4892ad39002b77900
Revika Alifia <revikasyariqatunalifia@gmail.com>
2025-01-29 20:14:58 +0700

    editbyrvk

--- user/pengajuanstatus.php
+++ user/pengajuanstatus.php
@@ -7,6 +7,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!--=============== CSS ===============-->
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    <link rel="stylesheet" href="../assets/css/lowongan.css" />
+    <link rel="stylesheet" href="../assets/css/home.css"/>
 </head>
 <body>
 <?php include "../assets/layout/navbarUser.php" ?>
