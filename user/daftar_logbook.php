ca12e50da67aa10f308c019a43aae8d1d62f7d95
Heviaa7 <duwidevi657@gmail.com>
2025-02-06 22:30:08 +0700

    edit by hevia

--- user/daftar_logbook.php
+++ user/daftar_logbook.php
@@ -11,28 +11,36 @@
             <thead>
                 <tr>
                     <th>No</th>
+                    <th>Tanggal</th>
+                    <th>Kegiatan</th>
+                    <th>Keterangan</th>
                     <th>Nama File</th>
-                    <th>Tanggal Unggah</th>
                     <th>Aksi</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>1</td>
-                    <td>Logbook_1.pdf</td>
                     <td>2025-02-03</td>
+                    <td>Meeting dengan tim</td>
+                    <td>Diskusi proyek magang</td>
+                    <td>Logbook_1.pdf</td>
                     <td><a href="../assets/doc/logbook_1.pdf" class="btn btn-primary btn-sm" target="_blank">Lihat</a></td>
                 </tr>
                 <tr>
                     <td>2</td>
-                    <td>Logbook_2.pdf</td>
                     <td>2025-02-02</td>
+                    <td>Pengerjaan tugas</td>
+                    <td>Mengerjakan laporan harian</td>
+                    <td>Logbook_2.pdf</td>
                     <td><a href="../assets/doc/logbook_2.pdf" class="btn btn-primary btn-sm" target="_blank">Lihat</a></td>
                 </tr>
                 <tr>
                     <td>3</td>
-                    <td>Logbook_3.pdf</td>
                     <td>2025-02-01</td>
+                    <td>Presentasi hasil</td>
+                    <td>Mempresentasikan laporan</td>
+                    <td>Logbook_3.docx</td>
                     <td><a href="../assets/doc/logbook_3.docx" class="btn btn-primary btn-sm" target="_blank">Lihat</a></td>
                 </tr>
             </tbody>
