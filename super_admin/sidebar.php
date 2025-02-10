e224e0ed90ef27cc197d1d9ab6a4d463752a8464
OyyHuss <mishbahus30@gmail.com>
2025-02-10 13:49:34 +0700

    edit by huss

--- super_admin/sidebar.php
+++ super_admin/sidebar.php
@@ -143,9 +143,9 @@
 
     
 
+    <script src="../assets/js/script.js"></script>
         <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
