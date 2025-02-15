2dfadf8e3a9f5e7dabd322fb50b64879fd97b5a8
Revika Alifia <revikasyariqatunalifia@gmail.com>
2025-02-15 20:37:51 +0700

    editbyrvk

--- user/kegiatan_aktif.php
+++ user/kegiatan_aktif.php
@@ -1,4 +1,19 @@
-<?php include "../layout/sidebarUser.php" ?>
+<?php
+include "../koneksi.php";
+
+include "../layout/sidebarUser.php";
+
+// Query hanya menampilkan data pengajuan magang berdasarkan id_user dari sesi
+$sql = "SELECT * FROM tb_pengajuan 
+        JOIN tb_profile_user ON tb_pengajuan.id_user = tb_profile_user.id_user
+        JOIN tb_pendidikan ON tb_profile_user.id_pendidikan = tb_pendidikan.id_pendidikan
+        JOIN tb_instansi ON tb_pengajuan.id_instansi = tb_instansi.id_instansi
+        JOIN tb_bidang ON tb_pengajuan.id_bidang = tb_bidang.id_bidang
+        WHERE tb_pengajuan.status_active = 'Y' 
+        AND tb_pengajuan.id_user = '$id_user'";  // Menampilkan hanya data user yang sedang login
+$query = mysqli_query($conn, $sql);
+$no = 1;
+?>
 
 <div class="main-content p-4">
     <div class="container-fluid">
@@ -16,7 +31,7 @@
                     <tr>
                         <th class="text-center">No</th>
                         <th class="text-center">Nama</th>
-                        <th class="text-center">Universitas</th>
+                        <th class="text-center">Pendidikan</th>
                         <th class="text-center">Perusahaan</th>
                         <th class="text-center">Bidang</th>
                         <th class="text-center">Durasi</th>
@@ -25,38 +40,39 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Data 1 -->
+                <?php while($row = mysqli_fetch_assoc($query)): ?> 
                     <tr>
-                        <td class="text-center">1</td>
-                        <td>Hendra Hartono</td>
-                        <td>Universitas Trunojoyo Madura</td>
-                        <td>Kominfo Sidoarjo</td>
-                        <td>Cyber Security</td>
-                        <td class="text-center">3 Bulan</td>
-                        <td>02 Januari - 02 Mei</td>
+                        <td class="text-center"><?= $no++; ?></td> 
+                        <td><?= $row['nama_user'] ?></td> 
+                        <td><?= $row['nama_pendidikan'] ?></td> 
+                        <td><?= $row['nama_panjang'] ?></td> 
+                        <td><?= $row['nama_bidang'] ?></td> 
                         <td class="text-center">
-                        <a href="detail_aktif.php" class="text-decoration-none" title="Lihat Detail">
-                        <i class="bi bi-eye" style="font-size: 20px;"></i></a></td>
+                            <?php 
+                                if (!empty($row['tanggal_mulai']) && !empty($row['tanggal_selesai'])) {
+                                    $start_date = new DateTime($row['tanggal_mulai']);
+                                    $end_date = new DateTime($row['tanggal_selesai']);
+                                    $interval = $start_date->diff($end_date);
+                                    echo $interval->m + ($interval->y * 12) . " Bulan";
+                                } else {
+                                    echo "Durasi Tidak Diketahui";
+                                }
+                            ?>
+                        </td>
+                        <td>
+                            <?= date('d F Y', strtotime($row['tanggal_mulai'])) . ' - ' . date('d F Y', strtotime($row['tanggal_selesai'])) ?>
+                        </td>
+                        <td class="text-center">
+                            <a href="detail_aktif.php" class="text-decoration-none" title="Lihat Detail">
+                                <i class="bi bi-eye" style="font-size: 20px;"></i>
+                            </a>
                         </td>
                     </tr>
-                    <!-- Data 2 -->
-                    <tr>
-                        <td class="text-center">2</td>
-                        <td>Hendra Hartono</td>
-                        <td>Universitas Trunojoyo Madura</td>
-                        <td>Kominfo Medan</td>
-                        <td>Website Developer</td>
-                        <td class="text-center">6 Bulan</td>
-                        <td>02 Januari - 02 Mei</td>
-                        <td class="text-center">
-                        <a href="detail_aktif.php" class="text-decoration-none" title="Lihat Detail">
-                        <i class="bi bi-eye" style="font-size: 20px;"></i></a></td>
-                        </td>
-                    </tr>
+                <?php endwhile; ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
 
-<?php include "../layout/footerDashboard.php" ?>
+<?php include "../layout/footerDashboard.php"; ?>
