655625653427a3fbbc0228d088043c4598a63dfb
Revika Alifia <revikasyariqatunalifia@gmail.com>
2025-04-17 09:42:10 +0700

    editbyrvk

--- user/persyaratan_daftar.php
+++ user/persyaratan_daftar.php
@@ -133,7 +133,14 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['hapus_laporan'])) {
             </table>
         </div>
     </div>
+    <div class="text-end mt-3">
+    <a href="status_pengajuan.php" class="btn btn-outline-secondary btn-sm">
+        <i class="bi bi-x-circle"></i> Tutup
+    </a>
 </div>
+
+</div>
+
 
 <!-- Proses Upload -->
 <?php 
@@ -188,10 +195,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     </div>
 </div>
 
+
 <!-- Bootstrap Bundle (termasuk Popper) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js"></script>
 
 <style>
