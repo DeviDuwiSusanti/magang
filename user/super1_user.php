cc4d33a4fa95cad97faaf38ec2033c6e86a357ca
OyyHuss <mishbahus30@gmail.com>
2025-06-09 14:58:24 +0700

    huss

--- user/super1_user.php
+++ user/super1_user.php
@@ -209,7 +209,7 @@ if (isset($_POST["edit_data_user"])) {
                         <input type="text" class="form-control" id="nama_lengkap" name="nama_user" required>
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-labNIK</label>
+                        <label for="email" class="form-label">Email</label>
                         <input type="email" class="form-control" id="email" name="email" required>
                     </div>
 
